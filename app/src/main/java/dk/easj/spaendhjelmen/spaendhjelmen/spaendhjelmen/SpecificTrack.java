fd1925345d96500fb139f3bea15be37c7077b75c
Benjamin Brobæk Lindgaard <benj2421@edu.easj.dk>
2018-11-05 14:09:41 +0100

    Progressbar

--- app/src/main/java/dk/easj/spaendhjelmen/spaendhjelmen/spaendhjelmen/SpecificTrack.java
+++ app/src/main/java/dk/easj/spaendhjelmen/spaendhjelmen/spaendhjelmen/SpecificTrack.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import dk.easj.spaendhjelmen.spaendhjelmen.R;
 
 public class SpecificTrack extends AppCompatActivity {
 private Track track;
-
+private TextView specific_track_information, specific_track_parkinginformation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +28,16 @@ private Track track;
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        specific_track_information = findViewById(R.id.specific_track_information);
+        specific_track_information.setText(track.info);
+
+        specific_track_parkinginformation = findViewById(R.id.specific_track_parkinginformation);
+        specific_track_parkinginformation.setText(track.parkInfo);
+
     }
 
 
+    public void mainFloatBtnClicked(View view) {
 
+    }
 }
