ec24ac874643a0afe513b362843521c16dd952fb
Magnus <magnusemortensen95@gmail.com>
2018-11-21 10:18:16 +0100

    SMS working

--- app/src/main/java/dk/easj/spaendhjelmen/spaendhjelmen/spaendhjelmen/Activities/GPSSecureActivity.java
+++ app/src/main/java/dk/easj/spaendhjelmen/spaendhjelmen/spaendhjelmen/Activities/GPSSecureActivity.java
@@ -1,12 +1,18 @@
 package dk.easj.spaendhjelmen.spaendhjelmen.spaendhjelmen.Activities;
 
+import android.Manifest;
 import android.app.Notification;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.Toast;
 
 import dk.easj.spaendhjelmen.spaendhjelmen.R;
 
@@ -15,6 +21,7 @@ import static dk.easj.spaendhjelmen.spaendhjelmen.spaendhjelmen.Activities.App.c
 public class GPSSecureActivity extends AppCompatActivity {
 
 private NotificationManagerCompat notificationManagerCompat;
+private static final int sendSmsPermissionsRequestCode = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,13 +42,30 @@ private NotificationManagerCompat notificationManagerCompat;
         notificationManagerCompat.notify(1,notification);
     }
 
+    public boolean CheckPermissions(String permission){
+        int check = ContextCompat.checkSelfPermission(this, permission);
+        return check == PackageManager.PERMISSION_GRANTED;
+    }
+
+
+
     public void gpsSecureTændSlukClicked(View view) {
         SendOnChannelAlert("TestTitle", "TestContext");
     }
 
     public void gpsSecureIndstillingerClicked(View view) {
+        SendAlertSMS();
     }
 
+    private void SendAlertSMS() {
+        if (CheckPermissions(Manifest.permission.SEND_SMS)){
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage("69",null,"message",null,null);
+        }
+        else{
+            ActivityCompat.requestPermissions(GPSSecureActivity.this,new String[]{Manifest.permission.SEND_SMS},sendSmsPermissionsRequestCode);
+        }
+    }
 
 
 }
